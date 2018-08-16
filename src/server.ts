f7c18a0ceb97f49072f55e44c5b165a2c505d3fb
Grzegorz Abramczyk <grzegorz.abramczyk@tuatara.pl>
2018-08-16 09:22:44 +0200

    Default html for vue history based navigation

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import * as express from 'express'
+import {cwd} from 'process'
 import {api} from './api'
 import {authenticate, security} from './security'
 const app = express()
@@ -7,5 +8,8 @@ app.use('/api', authenticate, api)
 app.use(express.static('web/dist'))
 app.use(express.static('static'))
 app.use(security)
+app.get('*', (req, res) => {
+    res.sendFile(`${cwd()}/static/index.html`)
+})
 
 app.listen(4000)
