61448543bfdfd3bf60048b248b96c70e568cd902
Grzegorz Abramczyk <grzegorz.abramczyk@tuatara.pl>
2018-08-17 16:05:02 +0200

    Single aspsp api

--- src/api.ts
+++ src/api.ts
@@ -7,3 +7,13 @@ api.get('/aspsps', (req, res) => {
     .then(({data}) => res.send(data))
     .catch((error) => res.status(500).send())
 })
+
+api.get('/aspsps/:aspspId', (req, res) => {
+    const aspspId = req.params.aspspId
+    Promise.all([
+        axios.get(`/metadata/aspsps/${aspspId}`, {baseURL: 'http://localhost:3000'}),
+        axios.get(`/consent/internal/request/${aspspId}`, {baseURL: 'http://localhost:3000'})
+    ])
+    .then(([{data: metadata}, {data: requests}]) => res.send({...metadata, requests}))
+    .catch((error) => res.status(500).send())
+})
