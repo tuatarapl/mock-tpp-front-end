c167cb9539130e64bcaa8273b412afa745528adc
Grzegorz Abramczyk <grzegorz.abramczyk@tuatara.pl>
2018-11-07 13:10:09 +0100

    Handling callbacks

--- src/callback.ts
+++ src/callback.ts
@@ -2,15 +2,18 @@ import axios from 'axios'
 import {raw} from 'body-parser'
 import {Router} from 'express'
 export const callback = Router()
+
+const baseURL = process.env.BACK_END_URL || 'http://localhost:8080'
 callback.use('/:endpoint', raw(), (req, res) => {
-    axios.post('/consent/internal/callback', {
-        body: req.body,
-        psuId: req.user.username,
-        method: req.method.toLowerCase(),
-        headers: req.headers,
-        url: `${req.protocol}://${req.host}${req.originalUrl}`,
-        endpoint: req.params.endpoint
-    }, {baseURL: 'http://localhost:3000'})
-    .then(({data}) => res.send(data))
+    axios.post('/callback', req.body, {
+        baseURL,
+        headers: {
+            'x-tuatara-psu-id': req.user.username,
+            'x-tuatara-endpoint': req.params.endpoint,
+            'x-tuatara-original-url': req.originalUrl,
+            'x-tuatara-original-method': req.method
+        }
+    })
+    .then(({data}) => res.redirect('/callbackResult'))
     .catch((error) => res.status(500).send())
 })
