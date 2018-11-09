7b2391fb0773a714763ef85165c623fb9b072345
Grzegorz Abramczyk <grzegorz.abramczyk@tuatara.pl>
2018-11-09 15:14:24 +0100

    Basic privlege list handling

--- web/src/components/consent.ts
+++ web/src/components/consent.ts
@@ -4,11 +4,27 @@ Vue.component('consent-edit', {
     template: `
 <form class="form-group">
     <div class="form-group">
-        <label for="session">Scope Group Type</label>
-        <select class="form-control" id="session" v-model="consent.scope_details.scopeGroupType"
+        <label for="scopeGroupType">Scope Group Type</label>
+        <select class="form-control" id="scopeGroupType" v-model="consent.scope_details.scopeGroupType"
             @change="consent.scope = $event.target.value">
             <option v-for="scopeGroupType in scopeGroupTypes">{{scopeGroupType}}</option>
         </select>
+    </div>
+    <div class="form-group">
+        <label for="privilegeList">Privilege List</label>
+        <ul class="list-group">
+            <li class="list-group-item" v-for="(privilege,index) in consent.scope_details.privilegeList">
+                <div class="form-group">
+                    <label :for="'accountNumber_'+index">Account Number</label>
+                    <input type="text" class="form-control" :id="'accountNumber_'+index"
+                        v-model="privilege.accountNumber"/>
+                </div>
+                <button type="button" class="btn btn-primary" @click="doDeletePrivilege(index)">Delete</button>
+            </li>
+            <li class="list-group-item">
+                <button type="button" class="btn btn-primary" @click="doAddPrivilege()">Add</button>
+            </li>
+        </ul>
     </div>
     <div class="form-group">
         <label for="scopeTimeDuration">Scope Time Limit</label>
@@ -37,6 +53,14 @@ Vue.component('consent-edit', {
             scopeGroupTypes: ['ais-accounts', 'ais', 'pis'],
             throttlingPolicies: ['psd2Regulatory'],
             scopes: ['ais-accounts', 'ais', 'pis']
+        }
+    },
+    methods: {
+        doAddPrivilege() {
+            this.consent.scope_details.privilegeList.push({})
+        },
+        doDeletePrivilege(index) {
+            this.consent.scope_details.privilegeList.splice(index, 1)
         }
     }
 })
