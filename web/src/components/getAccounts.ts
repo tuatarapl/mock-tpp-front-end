632cc27827ea7a17adfbd5af22678f26c57e2826
Grzegorz Abramczyk <grzegorz.abramczyk@tuatara.pl>
2018-11-09 08:50:41 +0100

    Get Account visualization

--- web/src/components/getAccounts.ts
+++ web/src/components/getAccounts.ts
@@ -3,23 +3,26 @@ import Vue from 'vue'
 Vue.component('get-accounts-result', {
     props: ['data'],
     template: `
-<table class="table table-striped">
-    <thead>
-        <tr>
-            <th>Account Number</th>
-            <th>Account Type Name</th>
-            <th>Account Type Code</th>
-            <th>Account Type Description</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr v-for="account in data.accounts">
-            <td>{{account.accountNumber}}</td>
-            <td>{{account.accountTypeName}}</td>
-            <td>{{account.accountType.code}}</td>
-            <td>{{account.accountType.description}}</td>
-        </tr>
-    </tbody>
-</table>
-`
-  })
+<div>
+    <result-header :header="data.responseHeader"></result-header>
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>Account Number</th>
+                <th>Account Type Name</th>
+                <th>Account Type Code</th>
+                <th>Account Type Description</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="account in data.accounts">
+                <td>{{account.accountNumber}}</td>
+                <td>{{account.accountTypeName}}</td>
+                <td>{{account.accountType.code}}</td>
+                <td>{{account.accountType.description}}</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+`})
+
--- web/src/components/results.ts
+++ web/src/components/results.ts
@@ -1,15 +1,33 @@
 import Vue from 'vue'
 
+const operationToComponent = {
+    getAccounts: 'get-accounts-result'
+}
+
 Vue.component('show-results', {
     props: ['data'],
-    template: '<component :is="currentComponent" :data="data"></component>',
+    template: `
+<div>
+    <h3>{{data.operation}}</h3>
+    <component :is="currentComponent" :data="data.response"></component>
+</div>
+`,
     computed: {
         currentComponent(): string {
-            return 'get-accounts-result'
+            return operationToComponent[this.data.operation]
         }
     }
 })
 Vue.component('result-header', {
     props: ['header'],
-    template: '<component ></component>'
+    template: `
+<dl class="row">
+    <dt class="col-sm-3 col-md-1">Request Id</dt>
+    <dd class="col-sm-9 col-md-4">{{header.requestId}}</dd>
+    <dt class="col-sm-3 col-md-1">Send Data</dt>
+    <dd class="col-sm-9 col-md-4">{{header.sendDate}}</dd>
+    <dt class="col-sm-3 col-md-1">Is Callback</dt>
+    <dd class="col-sm-9 col-md-1">{{header.isCallback}}</dd>
+</dl>
+`
 })
