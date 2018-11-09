e77192da383444da96b4f11d9a100be23353bd1f
Grzegorz Abramczyk <grzegorz.abramczyk@tuatara.pl>
2018-11-09 12:55:44 +0100

    Customizing bootstrap

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,5 +1,5 @@
 import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
 import 'jquery'
 import 'popper.js'
 import './app'
+import './bootstrap-custom.scss'
--- web/webpack.config.js
+++ web/webpack.config.js
@@ -16,6 +16,13 @@ module.exports = {
                 'css-loader'
             ]
         }, {
+            test: /\.scss$/,
+            use: [
+                "style-loader",
+                "css-loader", 
+                "sass-loader" 
+            ]
+        }, {
             test: /\.(png|svg|jpg|gif)$/,
             use: [
                 'file-loader'
